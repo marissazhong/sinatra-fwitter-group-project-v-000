4a56af13a01b6a00f3296a60a7c7d0f9981372a4
Marissa Zhong <mwz2101@columbia.edu>
2018-07-22 13:53:22 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   get '/users/:slug' do
-    @user = User.find_by_slug(params[:slug])
+    @user = User.find(session[:user_id])
     erb :'/users/show'
   end
 
