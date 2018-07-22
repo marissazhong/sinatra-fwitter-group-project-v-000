170581a18fe871da0bb0173f17ebb245373b6bd3
Marissa Zhong <mwz2101@columbia.edu>
2018-07-22 14:59:34 +0000

    automatically backed up by learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -73,6 +73,7 @@ class TweetsController < ApplicationController
   delete '/tweets/:id/delete' do
     if logged_in?
       @tweet = Tweet.find_by_id(params[:id])
+      @current_user = current_user
       if @tweet && @tweet.user_id == current_user.id
         @tweet.delete
       end
