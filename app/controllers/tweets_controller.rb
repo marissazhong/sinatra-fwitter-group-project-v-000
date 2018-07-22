283f50d5ebbc4499a76a2f681bc99baa322eadb5
Marissa Zhong <mwz2101@columbia.edu>
2018-07-22 14:19:41 +0000

    automatically backed up by learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -19,6 +19,7 @@ class TweetsController < ApplicationController
 
   get '/tweets' do
     if logged_in?
+      @user = current_user
       @tweets = Tweet.all
       erb :'/tweets/tweets'
     else
