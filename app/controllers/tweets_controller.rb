a3425a20f1d37dc24b0cab68e68d41cb4054c7da
Marissa Zhong <mwz2101@columbia.edu>
2018-07-22 13:27:52 +0000

    automatically backed up by learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,22 @@
 class TweetsController < ApplicationController
 
+  get '/tweets/new' do
+    erb :'/tweets/new'
+  end
+
+  get '/tweets/:id' do
+    @tweet = Tweet.find_by_id(params[:id])
+    erb :'/tweets/show'
+  end
+
+  get '/tweets' do
+    
+  end
+
+  post '/tweets' do
+    @tweet = Tweet.create(params[:tweet])
+    @tweet.save
+    redirect to "/tweets/#{tweet.id}"
+  end
 
 end
